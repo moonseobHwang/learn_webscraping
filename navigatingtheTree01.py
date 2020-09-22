4b95c8a1d98eaeec3bd97f78b07dd7bce3241afe
username <user@email.mail>
2020-09-22 17:55:03 +0900

    Update navigatingtheTree01.py

--- navigatingtheTree01.py
+++ navigatingtheTree01.py
@@ -4,14 +4,21 @@ import requests
 url = "https://www.google.com/search?q=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&oq=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&aqs=chrome..69i57.2150j0j15&sourceid=chrome&ie=UTF-8"
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
 res = requests.get(url=url, headers=header)
-
+soup = BeautifulSoup(res.content, 'html.parser')
+links = soup.select('a')
+b=0
+for link in links:
+    b=b+1
+    if b > 100:    
+        print(type(link),link)      
+        print('-'*100)
 #print(res.status_code, res.content)
-soup = BeautifulSoup(res.content, features='lxml')
-link = soup.body
-print(type(link), link.a.next_siblings)
-for sibling in link.children:
-    for sibling2 in sibling.div.next_siblings:
-        print(sibling2)
+# soup = BeautifulSoup(res.content, features='lxml')
+# link = soup.body
+# print(type(link), link.a.next_siblings)
+# for sibling in link.children:
+#     for sibling2 in sibling.div.next_siblings:
+#         print(sibling2)
 #     for sibling2 in link.div.next_siblings:
 #         if link.a.next_siblings== True:
 #             for sibling3 in link.a.next_siblings:
