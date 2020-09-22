b762d86906889707ea832de29a8d47a00220d4d3
username <user@email.mail>
2020-09-22 14:17:01 +0900

    Create navigatingtheTree.py

---
+++ navigatingtheTree.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+with open('datas/sample02.html') as fp:
+    soup = BeautifulSoup(fp, features="lxml")
+    # Going down - .children
+    bodychildren = soup.body.children
+    print(type(bodychildren), len(list(bodychildren)))
+    # <class 'list_iterator'>
+    body_tag = soup.body
+    for child in body_tag.children:             #child 사용자 정의 변수
+        print(type(child), repr(child))         #returns a printable representation
+        # <class 'bs4.element.NavigableSoup'>
+        # <class 'bs4.element.Tag><p class="title"><b>....
+
+    # Going up - .parent and .parents
+    title_tag = soup.title # <class "bs4.element.Tag"><title>The Dormouse's...</title>
+    print(title_tag.parent, title_tag.string.parent)
+    link=soup.p
+    print(type(link.parents), len(list(link.parents)))
+    # <class 'generator'> 4 -generator that behaves like an iterator
+    for parent in link.parents:                 #parent 사용자 정의 변수
+        print(type(parent.name), parent.name)
