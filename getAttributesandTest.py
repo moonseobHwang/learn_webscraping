363ae39bd8b10a4b59f74141c71fb6cc0d58075d
username <user@email.mail>
2020-09-23 14:20:56 +0900

    Update getAttributesandTest.py

--- getAttributesandTest.py
+++ getAttributesandTest.py
@@ -1,15 +1,22 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, bs4
 
-with open('datas/sample03.html') as fp:
-    soup = BeautifulSoup(fp, features='lxml')
+with open('datas/sample03.html',encoding='UTF8') as fp:
+    soup=BeautifulSoup(fp, features='lxml')
     title_data=soup.find('h1')
     print(type(title_data), title_data, title_data.string)
+    # <class 'bs4.element.Tag'> <h1 class="public_class_name" id="h1_id_name">[1]크롤링이란?</h1> [1]크롤링이란?
+
     title_data=soup.find_all(id='h1_id_name')
     print(title_data, title_data[0].get_text())
+    # [<h1 class="public_class_name" id="h1_id_name">[1]크롤링이란?</h1>] [1]크롤링이란?
+    
     title_data=soup.find_all('p', class_="public_class_name")
     print(title_data,title_data[0].attrs)
-    title_data=soup.find_all('p',attr={'align':'center'})
+    # [<p class="public_class_name" id="p01_id_name">웹페이지...것</p>] {'class': ['public_class_name'], 'id': 'p01_id_name'}
+
+    title_data=soup.find_all('p',id='p02_id_name')
     print(title_data,title_data[0].string)
+    
     title_data=soup.find_all('a',href=True)
     print(title_data, title_data[0].string)
